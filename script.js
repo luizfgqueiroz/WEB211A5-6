a483dfc7b92f6ba20419e6b3b829bd660676ec45
luizfgqueiroz <59620868+luizfgqueiroz@users.noreply.github.com>
2020-04-16 23:58:02 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 /** Add any JavaScript you need to this file. */
+displayOrderNum();
+displayOrderNum2();
+function displayOrderNum() {
+  var x = document.getElementById('orderNum');
+  if (x.style.display === 'none') x.style.display = 'block';
+  else x.style.display = 'none';
+}
+function displayOrderNum2() {
+  var x = document.getElementById('orderNum');
+  x.style.display = 'none';
+}
